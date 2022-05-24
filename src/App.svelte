7c33ab1d0613a6a361ac6725fd9de67d311cf53b
Yajat Vishwak <yajat472001@gmail.com>
2022-05-24 13:31:28 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Tailwind from "./Tailwind.svelte";
+	export let name;
+	
+</script>
+
+<main>
+	<Tailwind />
+	<table>
+		<tr class="text-4xl">bro</tr>
+	</table>
+</main>
+
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["src/**/*.svelte", "public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
